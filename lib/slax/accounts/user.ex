60f3bd2451d905143791cea41d3ca8abdecb992b
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2026-04-22 18:21:38 +0530

    add username field to users

--- lib/slax/accounts/user.ex
+++ lib/slax/accounts/user.ex
@@ -7,6 +7,7 @@ defmodule Slax.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :username, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :confirmed_at, :utc_datetime
--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -101,7 +101,7 @@ defmodule SlaxWeb.ChatRoomLive do
             </div>
           </div>
           <ul class="relative z-10 flex items-center gap-4 justify-end">
-            <li class="text-sm">{username(@current_scope.user)}</li>
+            <li class="text-sm">{@current_scope.user.username}</li>
             <li><.link href={~p"/users/settings"} class="text-sm font-semibold">Settings</.link></li>
             <li>
               <.link href={~p"/users/log-out"} method="delete" class="text-sm font-semibold">
@@ -264,7 +264,7 @@ defmodule SlaxWeb.ChatRoomLive do
       <div class="ml-2">
         <div class="-mt-1">
           <.link class="text-sm font-semibold hover:underline">
-            <span>{username(@message.user)}</span>
+            <span>{@message.user.username}</span>
           </.link>
           <span :if={@timezone} class="ml-1 text-xs text-gray-500">
             {message_timestamp(@message, @timezone)}
@@ -302,13 +302,9 @@ defmodule SlaxWeb.ChatRoomLive do
           <span class="w-2 h-2 rounded-full border-2 border-gray-500"></span>
         <% end %>
       </div>
-      <span class="ml-2 leading-none">{username(@user)}</span>
+       <span class="ml-2 leading-none">{@user.username}</span>
     </.link>
     """
-  end
-
-  defp username(user) do
-    user.email |> String.split("@") |> List.first() |> String.capitalize()
   end
 
   defp message_timestamp(message, timezone) do
